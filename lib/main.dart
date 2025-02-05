e9e7c0c29f43f37c5deee8f9952265647b264c98
Loobkom <fgc0874043287@gmail.com>
2025-02-05 22:38:13 +0700

    homework 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,257 +1,153 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Home'),
+      debugShowCheckedModeBanner: false,
+      home: AirQualityScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class AirQualityScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _AirQualityScreenState createState() => _AirQualityScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AirQualityScreenState extends State<AirQualityScreen> {
+  String city = "Loading...";
+  int aqi = 0;
+  double temperature = 0.0;
+  bool isLoading = true;
 
-  void _incrementCounter() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const SecondPage()),
-    );
+  Future<void> fetchAirQuality() async {
+    final url = Uri.parse(
+        "https://api.waqi.info/feed/here/?token=f66549ac4f2b58b32caf4057020df31572e12da3");
+
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+
+        if (data['status'] == "ok") {
+          setState(() {
+            city = data['data']['city']['name'];
+            aqi = data['data']['aqi'];
+            temperature =
+                data['data']['iaqi']['t']['v'].toDouble(); // ดึงค่าอุณหภูมิ
+            isLoading = false;
+          });
+        } else {
+          setState(() {
+            city = "Error";
+            isLoading = false;
+          });
+        }
+      } else {
+        setState(() {
+          city = "Error";
+          isLoading = false;
+        });
+      }
+    } catch (e) {
+      setState(() {
+        city = "Error";
+        isLoading = false;
+      });
+    }
+  }
+
+  Color getAqiColor(int aqi) {
+    if (aqi <= 50) {
+      return Colors.green;
+    } else if (aqi <= 100) {
+      return Colors.yellow;
+    } else if (aqi <= 150) {
+      return Colors.orange;
+    } else if (aqi <= 200) {
+      return Colors.red;
+    } else if (aqi <= 300) {
+      return Colors.purple;
+    } else {
+      return Colors.brown;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAirQuality();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(
+          "Air Quality Index (AQI)",
+          style: TextStyle(
+              fontSize: 25, color: const Color.fromARGB(255, 255, 255, 255)),
+        ),
+        backgroundColor: Colors.purple,
+        centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'กดปุ่มเพื่อไปหน้าถัดไป',
-              style: TextStyle(fontSize: 18),
-            ),
-            /*Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),*/
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Account Managementtttt'),
-      ),
-      body: Center(
-        child: Column(
+        child: isLoading
+            ? CircularProgressIndicator()
+            : Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Return to 1st page'),
+            Text(
+              city,
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const RegisterPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                  //backgroundColor: Color(16777215),
-                  ),
-              child: const Text('Register'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const LoginPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                  //backgroundColor: Color(8),
-                  ),
-              child: const Text('Login'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // สร้าง TextField สำหรับกรอก username และ password
-    final TextEditingController usernameController = TextEditingController();
-    final TextEditingController passwordController = TextEditingController();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Input Your Information',
-              style: TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-                border: OutlineInputBorder(),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: getAqiColor(aqi),
+                borderRadius: BorderRadius.circular(10),
               ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true, // ซ่อนรหัสผ่าน
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // แสดงข้อมูลที่กรอก
-                final username = usernameController.text;
-                final password = passwordController.text;
-
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(
-                      'Register Done!\nUsername: $username\nPassword: $password',
+              child: Column(
+                children: [
+                  Text(
+                    "$aqi",
+                    style: TextStyle(
+                      fontSize: 64,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
-                );
-                // ย้อนกลับไปหน้าก่อนหน้า
-                Navigator.pop(context);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-              ),
-              child: const Text('Register'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // สร้าง TextField สำหรับกรอก username และ password
-    final TextEditingController usernameController = TextEditingController();
-    final TextEditingController passwordController = TextEditingController();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Input Your Information',
-              style: TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true, // ซ่อนรหัสผ่าน
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // แสดงข้อมูลที่กรอก
-                final username = usernameController.text;
-                final password = passwordController.text;
-
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(
-                      'Login Done!\nUsername: $username',
-                    ),
+                  Text(
+                    aqi > 150 ? "Unhealthy" : "Good",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: const Color.fromARGB(255, 255, 255, 255)),
                   ),
-                );
-                // ย้อนกลับไปหน้าก่อนหน้า
-                Navigator.pop(context);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
+                ],
               ),
-              child: const Text('Login'),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Temperature: ${temperature.toStringAsFixed(1)}°C",
+              style: TextStyle(
+                  fontSize: 18,
+                  color: const Color.fromARGB(255, 0, 0, 0)),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: fetchAirQuality,
+              child: Text("Refresh"),
             ),
           ],
         ),
